6e9a5df98807866270bdd7f93004a3ef8d16f9ea
RomanKaptarUA <romankaptar82@gmail.com>
2024-11-06 20:29:00 +0200

    .

--- script.js
+++ script.js
@@ -18,11 +18,11 @@
 
 
 
-window.addEventListener(
-    'scroll', _.throttle(() => {
-        console.log('Scroll 300ms');
-    }, 3000),
-);
+// window.addEventListener(
+//     'scroll', _.throttle(() => {
+//         console.log('Scroll 300ms');
+//     }, 3000),
+// );
 
 // const output = document.querySelector('.output');
 // let scrollCouner = 0;
@@ -32,30 +32,74 @@ window.addEventListener(
 //     output.textContent = scrollCouner;
 // })
 
-const vanilaOutput = document.querySelector('.output.vanilla');
-const throttleOutput = document.querySelector('.output.throttle');
-const debounceOutput = document.querySelector('.output.debounce');
-const eventCounter = {
-    vanilla: 0,
-    throttle: 0,
-    debounce: 0,
+// const vanilaOutput = document.querySelector('.output.vanilla');
+// const throttleOutput = document.querySelector('.output.throttle');
+// const debounceOutput = document.querySelector('.output.debounce');
+// const eventCounter = {
+//     vanilla: 0,
+//     throttle: 0,
+//     debounce: 0,
+// }
+
+// document.addEventListener('scroll', () => {
+//     eventCounter.vanilla += 1;
+//     vanilaOutput.textContent = eventCounter.vanilla;
+// });
+
+// document.addEventListener('scroll',
+//     _.throttle(() => {
+//     eventCounter.throttle += 1;
+//     throttleOutput.textContent = eventCounter.throttle;
+//    })
+// )
+
+// document.addEventListener('scroll',
+//     _.debounce(() => {
+//     eventCounter.debounce += 1;
+//     debounceOutput.textContent = eventCounter.debounce;
+//    })
+// )
+
+
+
+
+
+// Потрібно затримати виконання функції при введенні тексту в поле input,
+//  щоб не перевантажувати сервер. Використовуйте метод throttle з бібліотеки lodash.
+//   Встановіть час затримки в мілісекундах, наприклад 500мс, і передайте функцію,
+//    яку потрібно виконати при введенні тексту.
+
+
+
+
+
+
+// const input = document.querySelector('#input');
+// document.addEventListener('input', _.throttle((event) => {
+// const text = event.target.value;
+
+// }));
+
+
+
+
+
+// function candleInput(event){
+// const text = event.target.value;
+// console.log('Відправка на сервер:', text);
+// }
+// const throttleInput = _.throttle(candleInput, 500);
+// const input = document.querySelector('#input');
+// input.addEventListener('input', throttleInput);
+
+
+
+const text = document.getElementById('text');
+const input = document.getElementById('input');
+
+function handleInput(ev){
+const handleText = ev.target.value;
+text.textContent = handletext;
 }
 
-document.addEventListener('scroll', () => {
-    eventCounter.vanilla += 1;
-    vanilaOutput.textContent = eventCounter.vanilla;
-});
-
-document.addEventListener('scroll',
-    _.throttle(() => {
-    eventCounter.throttle += 1;
-    throttleOutput.textContent = eventCounter.throttle;
-   })
-)
-
-document.addEventListener('scroll',
-    _.debounce(() => {
-    eventCounter.debounce += 1;
-    debounceOutput.textContent = eventCounter.debounce;
-   })
-)
+const debounceInput = _.debounce(handleInput, 500);
