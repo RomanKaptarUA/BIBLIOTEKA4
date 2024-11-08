f0b1512fed1e4a41434c33f5b9fabec256855e3d
RomanKaptarUA <romankaptar82@gmail.com>
2024-11-08 19:46:08 +0200

    .

--- CDN/script.js
+++ CDN/script.js
@@ -1,13 +1,49 @@
 // IntersectionObserver(callback, options)
 
-const option = {
-    rootMargin: '50px',
-    threshold: 0.25,
-};
-const onEntry = (entries, observer) => {
-    entries.forEach(entry => {
-        //   ...
-    })
+// const option = {
+//     rootMargin: '50px',
+//     threshold: 0.25,
+// };
+// const onEntry = (entries, observer) => {
+//     entries.forEach(entry => {
+//         //   ...
+//     })
+// }
+
+// const observed = new IntersectionObserver(onEntry, option);
+
+
+
+
+
+
+////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.1,
 }
 
-const observed = new IntersectionObserver(onEntry, option);
+const observer = new intersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            const img = entry.target;
+            img.src = img.dataset.src;
+            observer.unobserve();
+        }
+    })
+}, options);
+
+document.querySelectorAll('.lazy-image').forEach(img => {
+    observer.unobserve(img)
+})
